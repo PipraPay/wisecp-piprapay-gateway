b3d5bcba128113b9a43115f016a327e6b6a42955
Mouzh Hossain Shad <shadmouzhhossain@gmail.com>
2025-06-28 12:10:29 +0600

    Update config.php

--- piprapay/config.php
+++ piprapay/config.php
@@ -6,8 +6,8 @@ return [
         'logo'    => 'logo.png',
     ],
     'settings' => [
-        'api_key'              => '18685489668459dc4d63d91468915877210048053768459dc4d63db249079801',
-        'base_url'             => 'https://dev.piprapay.com',
+        'api_key'              => '',
+        'base_url'             => 'https://sandbox.piprapay.com',
         'currency_code'        => 'BDT',
         'commission_rate'      => '0',
         'force_convert_to'     => 4,
