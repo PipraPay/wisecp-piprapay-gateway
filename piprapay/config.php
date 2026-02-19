a1b720ba7b1b694ea9a5cc5b49d44224fe61933f
Mouzh Hossain Shad <shadmouzhhossain@gmail.com>
2026-02-20 03:00:49 +0600

    Update config.php

--- piprapay/config.php
+++ piprapay/config.php
@@ -6,8 +6,8 @@ return [
         'logo'    => 'logo.png',
     ],
     'settings' => [
-        'api_key'              => '4a523a708ca5503cce34a0f24c39843c9a192a569af693a20f',
-        'base_url'             => 'https://demo.piprapay.com/api',
+        'api_key'              => '',
+        'base_url'             => '',
         'piprapay_version'     => '1',
         'commission_rate'      => '0',
         'force_convert_to'     => 4,
